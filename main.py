0be64260629bd6d284a6f8de2da2cf49d4fd0e17
Daniel Wu <86576103+DanielWu18316@users.noreply.github.com>
2021-06-30 01:39:37 +0000

    made it better

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
-def search():
+def display_info(chosen_region):
+    f = open('countries.csv')
+    countries = f.readlines()
+    f.close()
+    for row in countries:
+        lines = row.split(',')
+        if chosen_region in lines[1]:
+            print(lines[0], lines[1], lines[2], lines[3], lines[10])
+
+
+def region_confirm():
+    regions = [
+        'Asia', 'Baltics', 'C.W. of Ind. States', 'Eastern Europe',
+        'Latin Amer. & Carib', 'Near East', 'Northern Africa', 'Oceania',
+        'Sub-Saharan Africa', 'Western Europe'
+    ]
     print("Regions:\n1. Asia\n2. Baltics\n3. C.W. of Ind. States\n4. Eastern Europe\n5. Latin America & Carib\n6. Near East\n7. Northern Africa\n8. Oceania\n9. Sub-Saharan Africa\n10. Western Europe")
     region = 0
     while region != "quit":
-      try:
+        try:
             region = (
                 input("\nEnter a number that corresponds with the region\n> "))
             if region == "quit":
                 print("Ending...")
-                break
-            print_region = region_list[int(region) - 1]
-            print("Selected region =>", print_region)
+            print_region = regions[int(region) - 1]
+            print("Selected region =>", print_region, "\n")
             display_info(print_region.upper())
-      except ValueError:
-        print("Must be integer")
-      except IndexError:
-        print("Does not exist")
-
-
-def display_info(region):
-    f = open('countries.csv')
-    lines = f.readlines()
-    f.close()
-    for row in lines:
-        if region in row:
-            print(row[:-1]) #test
-
-
-region_list = [
-    'Asia', 'Baltics', 'C.W. of Ind. States', 'Eastern Europe',
-    'Latin Amer. & Carib', 'Near East', 'Northern Africa', 'Oceania',
-    'Sub-Saharan Africa', 'Western Europe'
-]
-
-search()
+        except ValueError:
+            print("Must be integer")
+        except IndexError:
+            print("Does not exist")
+region_confirm()
